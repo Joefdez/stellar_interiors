47b4bab049bae0ce649243ce4268867c61eca4d4
Joseph  Fernández <joe@Josephs-MacBook-Pro.local>
2016-10-16 16:48:29 +0100

    First real code written. Functions added to physlib

--- libs/physlib.py
+++ libs/physlib.py
@@ -1,19 +1,44 @@
 import libraries
 
+#####      Physical constants in SI units  #####
+
+R=8.3144598                       # Gas constant (J K^-1 mol^-1)
+N_a=6.02214086E23                 # Avogradro's number
+sig=5.670373E-8                   # Stefan-Boltzmann constant (J m^-2 s^-2 K^-4
+m_H=1.67372E-27                   # Hydrogen mass (kg)
+G=6.67408E-11                     # Universal gravitational constant (m^3 kg^-1 s^-2=
+k_b=1.38064852E-23                # Boltzmann constant (JK^-1
+
+
+#####      Unit conversions     #####
+
+
 ######     Physical equations and transformations     #######
+
+# Saha equation
+
 
 
 # Ideal gas equation
 
-# Mean molecular mass
+def rho(T,P,mu)
+    return (mu*P)/(R*T)
 
-   # At 0 ionization
 
-   # With non-zero ionization
 
-# Degree of ionization
+# Mean molecular mass for neutral gas
 
-# Saha equation
+def mu_0(X,Y,Z)
+     return 1./(X+Y/4.+Z/2.)
+   
+# Ionization
+
+def E(X,Y,T)
+
+
+# Mean molecular mass for gas with non-zero ionization
+
+
 
 # pp nuclear energy generation rate
 
@@ -21,4 +46,4 @@ import libraries
 
 def is_radiative()
 
-
+    return

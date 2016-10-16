1600c33b24264f054695e67f8efe2aa6126c2c4d
Joseph  Fernández <joe@Josephs-MacBook-Pro.local>
2016-10-16 02:11:18 +0100

    First commit

---
+++ libs/physlib.py
@@ -0,0 +1,24 @@
+import libraries
+
+######     Physical equations and transformations     #######
+
+
+# Ideal gas equation
+
+# Mean molecular mass
+
+   # At 0 ionization
+
+   # With non-zero ionization
+
+# Degree of ionization
+
+# Saha equation
+
+# pp nuclear energy generation rate
+
+# Convective transport / Radiative transport criteria
+
+def is_radiative()
+
+

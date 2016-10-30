28ac396138f9d5f91a9e3f6d770ac7085fdc3909
joe <joe@joe-pc>
2016-10-30 15:40:52 +0000

    Line breaks added for readability and convention

--- libs/integrators.py
+++ libs/integrators.py
@@ -18,8 +18,11 @@ def RK4():
     """
     num = len(y)-1
     k1 = calc_rhs(r, Mr, P, L, T, rho, X, Y, mu)
-    k2 = calc_rhs(r + hh/2., Mr + hh/2.*k1[0], P + hh/2.*k1[1], L + hh/2.*k1[2], T + hh/2.*k1[3], X, Y, mu)
-    k3 = calc_rhs(r + hh/2., Mr + hh/2.*k2[0], P + hh/2.*k2[1], L + hh/2.*k2[2], T + hh/2.*k2[3], X, Y, mu)
-    k4 = calc_rhs(r + hh, Mr + hh*k3[0], P + hh*k3[1], L + hh*k3[2], T + hh*k3[3], X, Y, mu)
+    k2 = calc_rhs(r + hh/2., Mr + hh/2.*k1[0], P + hh/2.*k1[1],
+                                   L + hh/2.*k1[2], T + hh/2.*k1[3], X, Y, mu)
+    k3 = calc_rhs(r + hh/2., Mr + hh/2.*k2[0], P + hh/2.*k2[1],
+                                   L + hh/2.*k2[2], T + hh/2.*k2[3], X, Y, mu)
+    k4 = calc_rhs(r + hh, Mr + hh*k3[0], P + hh*k3[1],
+                                   L + hh*k3[2], T + hh*k3[3], X, Y, mu)
 
     return y + h/6. * (k1 + 2.*k2 + 2.*k3 + k4)
